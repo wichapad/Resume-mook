53f290746ea164dc80132f0cb64173f1bc1c5f9b
Wichapad Muenjumrad <guy_wichapad@hotmail.com>
2023-10-04 17:59:45 +0700

    update navbar

--- build/javascript/page.js
+++ build/javascript/page.js
@@ -1,58 +1,21 @@
 //All navbar menu
-let navMenu = document.querySelectorAll("li");
+const menuResume = document.querySelector(".menu-resume");
+const openNavbar = document.querySelector("#open-bars");
+const closeNavbar = document.querySelector("#close-bars");
+
+openNavbar.addEventListener("click", () => {
+  menuResume.style.transform = "translateY(0)";
+});
+
+closeNavbar.addEventListener("click", () => {
+  menuResume.style.transform = "translateY(-100%)";
+});
 // filter Category Portfolio
 const categories = document.getElementById("portfolio_selection");
 const itemCategory = document.querySelectorAll("#portfolio_works .port_work");
 //loadmore
 let loadMoreBtn = document.querySelector("#load_more");
 let currentItem = 6;
-
-//navigation bar mobile
-const mobileBar = document.getElementById("menu_mobile");
-const selectionMobile = document.querySelector(".menu_selection");
-
-//Change color Scrolling Navbar
-window.addEventListener("scroll", () => {
-  const navbar = document.getElementById("navbar_selection");
-  const scrollPosition = window.scrollY;
-  if (scrollPosition > 0) {
-    navbar.classList.add("navbar_BG");
-  } else {
-    navbar.classList.remove("navbar_BG");
-  }
-});
-
-mobileBar.addEventListener("click", () => {
-  if (selectionMobile.style.display === "none") {
-    selectionMobile.style.display = "flex";
-  } else {
-    selectionMobile.style.display = "none";
-  }
-});
-
-// .active navbar
-navMenu.forEach((e) => {
-  e.addEventListener("click", function () {
-    navMenu.forEach((menu) => menu.classList.remove("active"));
-    this.classList.add("active");
-  });
-});
-
-//event filter Category Portfolio
-categories.addEventListener("click", (e) => {
-  let selectCategory = e.target.getAttribute("value");
-
-  itemCategory.forEach((item) => {
-    let itemList = item.getAttribute("data-category");
-
-    if (selectCategory === "all" || selectCategory === itemList) {
-      item.style.display = "block";
-      loadMoreBtn.style.display = "none";
-    } else {
-      item.style.display = "none";
-    }
-  });
-});
 
 // event loadmore
 loadMoreBtn.addEventListener("click", () => {
