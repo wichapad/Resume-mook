011e662dc93faab661796aa3fa55355b4fe0bf9e
Wichapad Muenjumrad <guy_wichapad@hotmail.com>
2023-10-05 17:57:17 +0700

    ีupdate navbar

--- build/javascript/page.js
+++ build/javascript/page.js
@@ -1,5 +1,5 @@
 //All navbar menu
-const menuResume = document.querySelector(".menu-resume");
+const menuResume = document.querySelector(".menu-selection");
 const openNavbar = document.querySelector("#open-bars");
 const closeNavbar = document.querySelector("#close-bars");
 
@@ -8,14 +8,44 @@ openNavbar.addEventListener("click", () => {
 });
 
 closeNavbar.addEventListener("click", () => {
-  menuResume.style.transform = "translateY(-100%)";
+  if (window.innerWidth <= 768) {
+    menuResume.style.transform = "translateY(-100%)";
+  }
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth <= 768) {
+    menuResume.style.transform = "translateY(-100%)";
+  }
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 768) {
+    menuResume.style.transform = "translateY(0)";
+  }
 });
 // filter Category Portfolio
 const categories = document.getElementById("portfolio_selection");
 const itemCategory = document.querySelectorAll("#portfolio_works .port_work");
+
+categories.addEventListener("click", (e) => {
+  let selectCategory = e.target.getAttribute("value");
+
+  itemCategory.forEach((item) => {
+    let itemList = item.getAttribute("data-category");
+
+    if (selectCategory === "all" || selectCategory === itemList) {
+      item.style.display = "block";
+      loadMoreBtn.style.display = "none";
+    } else {
+      item.style.display = "none";
+    }
+  });
+});
+
 //loadmore
 let loadMoreBtn = document.querySelector("#load_more");
-let currentItem = 6;
+let currentItem = 3;
 
 // event loadmore
 loadMoreBtn.addEventListener("click", () => {
